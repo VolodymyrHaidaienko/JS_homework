0b68baf93680383c7c54227e5f0a1f7e21e97e92
VolodymyrHaidaienko <haidaienko.volodymyr@gmail.com>
2021-11-15 08:33:24 +0200

    JSLessons

---
+++ L1/lesson1.js
@@ -0,0 +1,114 @@
+// classwork
+// 1. Створити масив з 10 чисел (можна використовувати Math.random Та Math.floor). Скласти результат цих чисел в змінній result.
+// let a = [1,2,3,4,5,6,7,8,9,10]
+// let rezult = a[0]+a[1]+a[2]+a[3]+a[4]+a[5]+a[6]+a[7]+a[8]+a[9]
+// console.log(rezult)
+
+// 2. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр
+// let book = {
+//     назва:"",
+//     кількістьCторінок:0,
+//     жанр:""
+// }
+// console.log(book)
+
+// 3. Створити об'єкт book з наступними полями  - назва, кількість сторінок, жанр, автори
+// let book = {
+//     назва:"",
+//     кількістьCторінок:0,
+//     жанр:"",
+//     автори:""
+// }
+// console.log(book)
+
+// 4. Створити масив об'єктів book з наступними полями  - назва, кількість сторінок, жанр, автори. Вивести кожну книгу як окремий об'єкт
+// let book=[
+//     book1={назва:"", кількістьCторінок:5, жанр:"", автори:""},
+//     book2={назва:"", кількістьCторінок:0, жанр:"", автори:""}
+// ]
+// console.log(book[0])
+
+// 5. Напишіть код, який вираховує площу прямокутника висотою 23см. (змінна height) та шириною 10см (змінна width).
+// Значення площі зберігати в змінній s.
+// let height = 23
+// let width = 10
+// let s = height*width
+// console.log(s)
+
+// 6. Напиши код, який знаходить об'єм циліндра заввишки 10м (змінна heightC) та діаметром основи 4м (dC),
+// результат помістіть у змінну v.
+
+// let heightC = 10
+// let dc = 4
+// let v = ((Math.PI)*(Math.pow(dc, 2)))*heightC
+// console.log(v)
+
+
+// 7. У прямокутного трикутника дві сторони n (зі значенням 3) та m (зі значенням 4).
+// Знайдіть гіпотенузу k за теоремою Піфагора (потрібно використовувати функцію Math.pow (число, ступінь) або оператор зведення в ступінь **).
+// let n = 3
+// let m = 4
+// let k = (Math.pow(n, 2))+(Math.pow(m, 2))
+// console.log(k)
+
+
+
+// homework
+
+// 1- Створити змінні. Присвоїти кожному з них значення: 'hello','owu','com', 'ua', 1, 10, -999, 123, 3.14, 2.7, 16, true, false.
+// let a = "hello"
+// console.log(a)
+// let b = "owu"
+// console.log(b)
+// let c = "com"
+// console.log(c)
+// let d = "ua"
+// console.log(d)
+// let i = 1
+// console.log(i)
+// let e = 10
+// console.log(e)
+// let f = -999
+// console.log(f)
+// let j = 123
+// console.log(j)
+// let p = 3.14
+// console.log(p)
+// let o = 2.7
+// console.log(o)
+// let n = 16
+// console.log(n)
+// let m = true
+// console.log(m)
+// let s = false
+// console.log(s)
+
+// Створити 3 змінних firstName, middleName, lastName, наповнити їх своїм ПІБ. З'єднати їх в одну змінну person. (Не об'єкт)
+// let firstName = "Гайдаєнко "
+// let middleName = "Володимир "
+// let lastName = "Миколайович"
+// let person = firstName+middleName+lastName
+// console.log(person)
+
+// За допомогою оператора typeof визначити типи наступних змінних та вивести їх в консоль.
+// let a = 100;
+// console.log(typeof(a))
+// let b = '100';
+// console.log(typeof (b))
+// let c = true;
+// console.log(typeof (c))
+
+
+// В однакових виразах не використовувати однакові оператори!!!
+// 5 < 6 -> true
+// 5 > 6 -> false
+// 5 === 6 -> false
+// 5 >== 6 -> false
+// 10 === 10 -> true
+// 10 >== 10 -> true
+// 10 > 10 -> false
+// 10 < 10 -> false
+// 10 !== 10 -> false
+// 123 === '123' -> false
+// 123 !== '123' -> true
+
